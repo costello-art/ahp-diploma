cf7601a7046723ad8dacc38c844d91dc5a46c820
Sviat Costigan <costello@users.noreply.github.com>
2014-11-19 01:41:00 +0200

    bug fixes now it's possible to enter target matrix for best actors

--- src/main/java/dialog/ActorsTargetsInput.java
+++ src/main/java/dialog/ActorsTargetsInput.java
@@ -2,6 +2,7 @@ package dialog;
 
 import model.Actor;
 import model.GlobalTarget;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,12 +14,16 @@ import java.util.ArrayList;
  * Created by Sviat on 19.11.14.
  */
 public class ActorsTargetsInput extends JDialog {
+    private static MigLayout migLayout = new MigLayout("ins 0, hidemode 3", "", "[][][]");
+
     private JLabel labelActorsTargetsInput;
     private JButton buttonNextActorTargetsInput;
     private JPanel panelActorsTargets;
     private JPanel panelRoot;
+    private JLabel labelMatrixForActor;
     private GlobalTarget target;
     private final ArrayList<Actor> bestActorsList;
+    private RTable table;
 
 
     public ActorsTargetsInput(StartWindow startWindow, GlobalTarget target) {
@@ -28,6 +33,8 @@ public class ActorsTargetsInput extends JDialog {
         setSize(new Dimension(640, 480));
         setContentPane(panelRoot);
 
+        panelActorsTargets.setLayout(migLayout);
+
         bestActorsList = target.getBestActors();
 
         labelActorsTargetsInput.setText(
@@ -36,7 +43,9 @@ public class ActorsTargetsInput extends JDialog {
                         bestActorsList.get(1).getName())
         );
 
+        labelMatrixForActor.setText("Матриця цілей для актора " + bestActorsList.get(0));
         buttonNextActorTargetsInput.addActionListener(new OnActorsMatrixEntered());
+        buildMatrixForCurrentActor(0);
     }
 
     public GlobalTarget display() {
@@ -49,16 +58,32 @@ public class ActorsTargetsInput extends JDialog {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            bestActorsList.get(currentActor).setMatrix(table.getMatrix());
+
+            //лічильник збільшується тут, бо на початку вручну обробили 0
+            currentActor++;
+
+            if (currentActor >= 2) {
+                buttonNextActorTargetsInput.setText("Матриця цілей для всіх акторів введена.");
+                buttonNextActorTargetsInput.setEnabled(false);
+                return;
+            }
+
+            labelActorsTargetsInput.setText("Матриця цілей для актора " + bestActorsList.get(currentActor).getName());
             buildMatrixForCurrentActor(currentActor);
         }
     }
 
     private void buildMatrixForCurrentActor(int currentActor) {
+        panelActorsTargets.removeAll();
+        panelActorsTargets.validate();
+        panelActorsTargets.repaint();
+
         ArrayList<String> targets = bestActorsList.get(currentActor).getTargets();
-        RTable table = new RTable(targets.size());
+        table = new RTable(targets.size());
         table.setHeaders(targets);
 
-        panelActorsTargets.add(table, "w: 50%, h:50% wrap");
+        panelActorsTargets.add(table, "w 100%, h 100%");
         panelActorsTargets.validate();
         panelActorsTargets.repaint();
     }
