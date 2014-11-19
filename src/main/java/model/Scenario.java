82fbe122e1218bccca3ce025d06ef9424bf93723
Sviat Costigan <costello@users.noreply.github.com>
2014-11-19 17:19:58 +0200

    Added scenario

---
+++ src/main/java/model/Scenario.java
@@ -0,0 +1,29 @@
+package model;
+
+import model.math.Calculate;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sviat on 19.11.14.
+ */
+public class Scenario {
+    private String name;
+
+    private float[][] matrix;
+    private ArrayList<Float> selfVector;
+
+
+    public Scenario(String name) {
+        this.name = name;
+    }
+
+    public void setMatrix(float[][] matrix) {
+        this.matrix = matrix;
+        selfVector = Calculate.selfVectorForMatrix(this.matrix);
+    }
+
+    public ArrayList<Float> getSelfVector() {
+        return selfVector;
+    }
+}
