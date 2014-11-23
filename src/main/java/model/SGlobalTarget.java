97e450c48922fdf70591d58187d2e7c2d4bed476
Sviat Costigan <costello@users.noreply.github.com>
2014-11-23 02:42:03 +0200

    Added new global target. Separate from first algo

---
+++ src/main/java/model/SGlobalTarget.java
@@ -0,0 +1,114 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sviat on 23.11.14.
+ */
+public class SGlobalTarget {
+
+    private String name;
+
+    private ArrayList<String> actors = new ArrayList<>();
+    private ArrayList<String> targets = new ArrayList<>();
+    private ArrayList<String> scenarios = new ArrayList<>();
+
+    private double[][] actorsMatrix;
+    private ArrayList<Double> actorsSelfVector = new ArrayList<>();
+
+    private ArrayList<double[][]> targetsMatrices = new ArrayList<>();
+    private ArrayList<double[][]> scenariosMatrices = new ArrayList<>();
+
+    private double[][] matrixFromScenariosSelfVectors;
+    private double[][] matrixFromTargetsSelfVectors;
+
+    private ArrayList<Double> result;
+
+    public SGlobalTarget(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getActors() {
+        return actors;
+    }
+
+    public void setActors(ArrayList<String> actors) {
+        this.actors = actors;
+    }
+
+    public ArrayList<String> getTargets() {
+        return targets;
+    }
+
+    public void setTargets(ArrayList<String> targets) {
+        this.targets = targets;
+    }
+
+    public ArrayList<String> getScenarios() {
+        return scenarios;
+    }
+
+    public void setScenarios(ArrayList<String> scenarios) {
+        this.scenarios = scenarios;
+    }
+
+    public double[][] getActorsMatrix() {
+        return actorsMatrix;
+    }
+
+    public void setActorsMatrix(double[][] actorsMatrix) {
+        this.actorsMatrix = actorsMatrix;
+    }
+
+    public ArrayList<Double> getActorsSelfVector() {
+        return actorsSelfVector;
+    }
+
+    public void setActorsSelfVector(ArrayList<Double> actorsSelfVector) {
+        this.actorsSelfVector = actorsSelfVector;
+    }
+
+    public ArrayList<double[][]> getTargetsMatrices() {
+        return targetsMatrices;
+    }
+
+    public void setTargetsMatrices(ArrayList<double[][]> targetsMatrices) {
+        this.targetsMatrices = targetsMatrices;
+    }
+
+    public ArrayList<double[][]> getScenariosMatrices() {
+        return scenariosMatrices;
+    }
+
+    public void setScenariosMatrices(ArrayList<double[][]> scenariosMatrices) {
+        this.scenariosMatrices = scenariosMatrices;
+    }
+
+    public double[][] getMatrixFromScenariosSelfVectors() {
+        return matrixFromScenariosSelfVectors;
+    }
+
+    public void setMatrixFromScenariosSelfVectors(double[][] matrixFromScenariosSelfVectors) {
+        this.matrixFromScenariosSelfVectors = matrixFromScenariosSelfVectors;
+    }
+
+    public double[][] getMatrixFromTargetsSelfVectors() {
+        return matrixFromTargetsSelfVectors;
+    }
+
+    public void setMatrixFromTargetsSelfVectors(double[][] matrixFromTargetsSelfVectors) {
+        this.matrixFromTargetsSelfVectors = matrixFromTargetsSelfVectors;
+    }
+
+    public void calculate() {
+        result = null;
+    }
+}
